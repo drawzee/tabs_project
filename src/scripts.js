3280b346a42d10b2dc3ee8c83f808131014836da
Nick <primizenkin.na@gmail.com>
2021-03-12 17:41:20 +0300

    script added

---
+++ src/scripts.js
@@ -0,0 +1,27 @@
+let activateTab = function () {
+    let tabBtn = document.querySelectorAll('[data-tab-target]');
+    let tabContent = document.querySelectorAll('[data-tab-content]');
+
+    tabBtn.forEach((item) => {
+        item.addEventListener('click', selectTab)
+    });
+
+    function selectTab() {
+        tabBtn.forEach((item) => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+        activeTab = this.getAttribute('data-tab-target');
+        selectContent(activeTab);
+    }
+    
+    function selectContent(activeTab) {
+        tabContent.forEach((item) => {
+           item.classList.contains(activeTab) ? 
+           item.classList.add('active') : 
+           item.classList.remove('active');
+        });
+    }
+};
+
+activateTab();
