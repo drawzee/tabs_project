186a769605e6283aacd96f147fa446b253fdffb8
Nick <primizenkin.na@gmail.com>
2021-06-18 16:03:14 +0300

    img upload & some fixes

--- src/scripts.js
+++ src/scripts.js
@@ -30,9 +30,47 @@ activateTab();
 
 
 let getUserName = function () {
-    let userName = document.getElementById("user_input").value;
-    user_name.innerHTML = 'Nice to meet you, ' + userName + '!';
-    if (!userName) {
-        user_name.innerHTML = 'What\'s your name again?';
-    }
+    let submitBtn = document.getElementById("submitBtn");
+    
+    submitBtn.addEventListener("click", function () {
+
+        let userName = document.getElementById("user_input").value;
+        user_name.innerHTML = 'Nice to meet you, ' + userName + '!';
+        if (!userName) {
+            user_name.innerHTML = 'What\'s your name again?';
+        }
+    });
 };
+
+getUserName();
+
+
+let getImg = function () {
+    let userImg = document.getElementById("userImg");
+    let imageContainer = document.querySelector(".image-container");
+    let imagePreview = imageContainer.querySelector(".user-image-preview");
+    let imageDefault = imageContainer.querySelector(".image-default");
+
+    userImg.addEventListener("change", function () {
+        let file = this.files[0];
+
+        if (file) {
+            let reader = new FileReader();
+            imageDefault.style.display = "none";
+            imagePreview.style.display = "block";
+
+            reader.addEventListener("load", function () {
+                imagePreview.setAttribute("src", this.result);
+            })
+
+            reader.readAsDataURL(file);
+            console.log(file);
+        } else {
+            imageDefault.style.display = null;
+            imagePreview.style.display = null;
+            imagePreview.setAttribute("src", "");
+        }
+    });
+};
+
+getImg();
